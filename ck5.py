2c4804e175b3b7fef9067197a866ad9ce18f80dc
SUNIL-KUDUPUDI-1644 <40815130+SUNIL-KUDUPUDI-1644@users.noreply.github.com>
2018-07-04 21:55:54 +0530

    Create ck5.py

---
+++ ck5.py
@@ -0,0 +1,9 @@
+a=int(input("enter the 1st no="))
+b=int(input("enter the 2nd no="))
+c=int(input("enter the 3rd no="))
+if a>b and a>c:
+    print("a is largest")
+elif b>a and b>c:
+    print("b is largest")
+else:
+    print("c is largest")
