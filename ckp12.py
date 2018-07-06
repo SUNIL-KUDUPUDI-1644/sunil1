df5994e9599dce33feac95ff7c327ed3736e77fe
SUNIL-KUDUPUDI-1644 <40815130+SUNIL-KUDUPUDI-1644@users.noreply.github.com>
2018-07-06 22:13:09 +0530

    Update ckp12.py

--- ckp12.py
+++ ckp12.py
@@ -13,7 +13,7 @@ for j in range(0,q):
     v=int(input("enter v"))
     h.append(v)
 for k in range(0,q):
-    t=s[g[k]:h[k]]
+    t=s[(g[k]-1):(h[k])]
     print(t)
     num=0
     for l in range(0,len(t)):
