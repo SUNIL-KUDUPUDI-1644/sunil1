0b0fa2e608cef27de52ac2f984e8501bba378921
SUNIL-KUDUPUDI-1644 <40815130+SUNIL-KUDUPUDI-1644@users.noreply.github.com>
2018-07-06 12:49:21 +0530

    Update ckp2.py

--- ckp2.py
+++ ckp2.py
@@ -13,27 +13,22 @@ e=len(s)
 print(e)
 print(s)
 r=int(input("enter digits to remove"))
-for j in range(0,r):
-    print("element to remove")
-    n2=int(input("enter the no."))
-    t.append(n2)
+n3=e-r
+t=s[:n3]
 print(t)
-y=list(set(s)-set(t))
-print(y)
 swap=0
-for m in range(0,len(y)):
-    for n in range(m+1,len(y)):
-        if(y[m]>y[n]):
-            swap=y[m]
-            y[m]=y[n]
-            y[n]=swap
-x=len(y)
-print(y)
+for m in range(0,len(t)):
+    for n in range(m+1,len(t)):
+        if(t[m]>t[n]):
+            swap=t[m]
+            t[m]=t[n]
+            t[n]=swap
+x=len(t)
+print(t)
 num=0
 g=x-1
 for z in range(0,x):
-    d=y[z]*(10**g)
-    print(d)
+    d=t[z]*(10**g)
     num=num+d
     g=g-1
 print(num)
