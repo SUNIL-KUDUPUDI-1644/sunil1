86eaf94a0d9dcc26480af34ccaceccf62e39366c
SUNIL-KUDUPUDI-1644 <40815130+SUNIL-KUDUPUDI-1644@users.noreply.github.com>
2018-07-10 13:34:17 +0530

    Update ckh5.py

--- ckh5.py
+++ ckh5.py
@@ -1,14 +1,14 @@
-w=int(input("enter the no."))
+w=int(input())
 s=[]
 q=[]
 a=[]
-for i in range(0,w):
-    s.append(int(input("enter the no.")))
-print(s)
+b=input()
+s.extend([int(x) for x in b.split()])
 for j in range(0,w):
     for k in range(j+1,w):
         if(s[j]==s[k]):
             q.append(s[j])
-print(q)
 a=set(s)-set(q)
-print(a)        
+z=list(a)
+for l in range(0,len(z)):
+    print(z[l],end=" ")
