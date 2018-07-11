68cdb7851d99b4402c84846fca1207e1f2a1fc03
SUNIL-KUDUPUDI-1644 <40815130+SUNIL-KUDUPUDI-1644@users.noreply.github.com>
2018-07-11 20:53:24 +0530

    Update ckh10.py

--- ckh10.py
+++ ckh10.py
@@ -1,17 +1,15 @@
-print("first no. should be greater than 2nd")
-w=int(input("enter the 1st no."))
-e=int(input("enter the 2nd no."))
+w,e=input().split()
+w=int(w)
+e=int(e)
 if(w>e):    
     s=[]
     q=[]
     a=[]
     f=[]
-    for i in range(0,w):
-        s.append(int(input("enter the 1st array")))
-    print(s)
-    for j in range(0,e):
-        q.append(int(input("enter the 2nd array")))
-    print(q)
+    b=input()
+    s.extend([int(x) for x in b.split()])
+    c=input()
+    q.extend([int(x) for x in c.split()])
     for m in q:
         if m not in s:
             f.append(m)
@@ -21,11 +19,7 @@ if(w>e):
                 if(q[k]==s[l]):
                     a.append(s[l])
         if(len(a)<=len(s)):
-            print("yes")
-        else:
-            print("no")
+            print("YES")
     else:
-        print("not subset")
-else:
-    print("enter again")
+        print("NO")
 
