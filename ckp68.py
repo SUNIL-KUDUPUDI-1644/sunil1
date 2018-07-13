47be04c840514cd0888b3111c5551d6ac048e178
SUNIL-KUDUPUDI-1644 <40815130+SUNIL-KUDUPUDI-1644@users.noreply.github.com>
2018-07-13 09:42:02 +0530

    Create ckp68.py

---
+++ ckp68.py
@@ -0,0 +1,12 @@
+a,b=input().split()
+if(a.isdigit() and b.isdigit()):
+    a=int(a)
+    b=int(b)
+    if(b==1):
+        print("1 2")
+    else:
+       print(str("1 "+str(a-b)))
+else:
+    print("invalid")
+    
+
