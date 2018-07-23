7898b3180afa48d32b637ddd30f39852301dfee1
SUNIL-KUDUPUDI-1644 <40815130+SUNIL-KUDUPUDI-1644@users.noreply.github.com>
2018-07-23 13:23:09 +0530

    Create ckp60.py

---
+++ ckp60.py
@@ -0,0 +1,16 @@
+a=int(input())
+j=4
+k=j
+z=0
+for i in range(1,a+1):
+    j=j-1
+    if(i==a):
+        print(j)
+    if(j==1):
+        z=z+1
+        l=(k-1)
+        p=2**(z)
+        j=(l*p)+1
+
+        
+    
