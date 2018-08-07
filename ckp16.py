500aed3fef99aa1553ed2951757c5571b29fde1b
SUNIL-KUDUPUDI-1644 <40815130+SUNIL-KUDUPUDI-1644@users.noreply.github.com>
2018-08-07 11:08:54 +0530

    Update ckp16.py

--- ckp16.py
+++ ckp16.py
@@ -2,7 +2,6 @@ b=int(input())
 j=[]
 c=input()
 j.extend([int(x) for x in c.split()])
-print(j)
 for z in range(0,len(j)):
     for k in range(z+1,len(j)):
         if(j[z]>j[k]):
@@ -11,7 +10,6 @@ for z in range(0,len(j)):
             j[k]=swap
 d=0
 p=0
-print(j)
 for l in range(1,len(j)):
     if(j[l-1]==j[l]):
         p=p+d
